bdf6588e6bf9e997831ab9b73b10570f906f7ab0
KCampbell22 <ksoup12@gmail.com>
2022-04-29 23:18:09 -0500

    Almost done

--- apps.js
+++ apps.js
@@ -1,10 +1,3 @@
-function showNav() {
-    let x = document.getElementsByClassName('nav-links');
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  console.log(showNav());
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
+}
